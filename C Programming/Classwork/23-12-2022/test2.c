d6d66066dcaa8af5e152cbfd034d3f7ca1566777
RKPatel24 <kaneriyaraj133@gmail.com>
2022-12-23 10:56:28 +0530

    c programming

---
+++ C Programming/Classwork/23-12-2022/test2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+
+{
+	int no = 29;
+	char ch = 'u';
+	float per = 58.78;
+	double stock = 784456225444.154645445;
+	char name[12]="top tech";
+	char mobile[12]= "7202009988";
+
+	
+	printf("%d\n",no);
+	printf("%c\n",ch);
+	printf("%.2f",per);
+	printf("\n%lf",stock);
+	printf("%s\n",name);
+	printf("%s\n",mobile);
+}
